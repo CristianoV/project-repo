6bd68677cb333c7934fd84474b2596d63acc06e1
CristianoV <cristianoviieira@gmail.com>
2023-01-02 19:11:59 -0300

    feat: busca informações dos repositorios

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,5 +1,32 @@
-export default function Repositorio() {
-  return (
-    <h1>Repositorio</h1>
-  );
+import { useEffect, useState } from 'react';
+import { Container } from './styles';
+import api from '../../services/api';
+
+export default function Repositorio({ match }) {
+  const [repositorio, setRepositorio] = useState({});
+  const [issues, setIssues] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadData() {
+      const repoName = decodeURIComponent(match.params.repository);
+
+      const [repositorioData, issuesData] = await Promise.all([
+        api.get(`/repos/${repoName}`),
+        api.get(`/repos/${repoName}/issues`, {
+          params: {
+            state: 'open',
+            per_page: 5,
+          },
+        }),
+      ]);
+
+      setRepositorio(repositorioData.data);
+      setIssues(issuesData.data);
+      setLoading(false);
+    }
+    loadData();
+  }, [match.params.repository]);
+
+  return <Container>Repositorio</Container>;
 }
