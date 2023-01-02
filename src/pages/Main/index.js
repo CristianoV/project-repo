f2b5918aa4a50aca0386ce9269d9d13e5db39f9d
CristianoV <cristianoviieira@gmail.com>
2023-01-02 16:39:03 -0300

    feat: adiciona função que procura respositorios no github

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,29 @@
 import { Container, Form, SubmitButton } from './styles';
 import { FaGithub, FaPlus } from 'react-icons/fa';
+import { useCallback, useState } from 'react';
+
+import api from '../../services/api';
 
 export default function Main() {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState([]);
+
+  const handleSubmit = useCallback((e) => {
+    e.preventDefault();
+
+    async function submit() {
+      const response = await api.get(`/repos/${newRepo}`);
+
+      const data = {
+        name: response.data.full_name,
+      };
+
+      setRepositories([...repositories, data]);
+      setNewRepo('');
+    }
+    submit();
+  }, [newRepo, repositories]);
+
   return (
     <Container>
       <h1>
@@ -9,10 +31,15 @@ export default function Main() {
         Meus Repositorios
       </h1>
 
-      <Form>
-        <input type="text" placeholder="Adicionar Repositorio" />
+      <Form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='Adicionar Repositorio'
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+        />
         <SubmitButton>
-          <FaPlus color="#fff" size={14} />
+          <FaPlus color='#fff' size={14} />
         </SubmitButton>
       </Form>
     </Container>
