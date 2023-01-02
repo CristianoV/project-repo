ac9e44fc1d06cc4c3f7909075862f88c4307bd9e
CristianoV <cristianoviieira@gmail.com>
2023-01-02 19:49:21 -0300

    feat: adiciona renderização de issues dos repositorios

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Container, Owner, Loading, BackButton } from './styles';
+import { Container, Owner, Loading, BackButton, IssuesList } from './styles';
 import { FaArrowLeft } from 'react-icons/fa';
 import api from '../../services/api';
 
@@ -40,16 +40,31 @@ export default function Repositorio({ match }) {
   return (
     <Container>
       <BackButton to='/'>
-        <FaArrowLeft color='#000' size={30}/>
+        <FaArrowLeft color='#000' size={30} />
       </BackButton>
       <Owner>
-        <img
-          src={repositorio.owner.avatar_url}
-          alt={repositorio.owner.login}
-        />
+        <img src={repositorio.owner.avatar_url} alt={repositorio.owner.login} />
         <h1>{repositorio.name}</h1>
         <p>{repositorio.description}</p>
       </Owner>
+
+      <IssuesList>
+        {issues.map((issue) => (
+          <li key={String(issue.id)}>
+            <img src={issue.user.avatar_url} alt={issue.user.login} />
+            <div>
+              <strong>
+                <a href={issue.html_url}>{issue.title}</a>
+
+                {issue.labels.map((label) => (
+                  <span key={String(label.id)}>{label.name}</span>
+                ))}
+              </strong>
+              <p>{issue.user.login}</p>
+            </div>
+          </li>
+        ))}
+      </IssuesList>
     </Container>
   );
 }
