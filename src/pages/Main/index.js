f1f19ccc8de963ef4093122deb1e56babcb71a6e
CristianoV <cristianoviieira@gmail.com>
2023-01-02 16:53:27 -0300

    feat: personalização button submit

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,5 @@
 import { Container, Form, SubmitButton } from './styles';
-import { FaGithub, FaPlus } from 'react-icons/fa';
+import { FaGithub, FaPlus, FaSpinner } from 'react-icons/fa';
 import { useCallback, useState } from 'react';
 
 import api from '../../services/api';
@@ -7,22 +7,33 @@ import api from '../../services/api';
 export default function Main() {
   const [newRepo, setNewRepo] = useState('');
   const [repositories, setRepositories] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  const handleSubmit = useCallback((e) => {
-    e.preventDefault();
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
 
-    async function submit() {
-      const response = await api.get(`/repos/${newRepo}`);
+      async function submit() {
+        setLoading(true);
+        try {
+          const response = await api.get(`/repos/${newRepo}`);
 
-      const data = {
-        name: response.data.full_name,
-      };
+          const data = {
+            name: response.data.full_name,
+          };
 
-      setRepositories([...repositories, data]);
-      setNewRepo('');
-    }
-    submit();
-  }, [newRepo, repositories]);
+          setRepositories([...repositories, data]);
+          setNewRepo('');
+        } catch (err) {
+          console.log(err);
+        } finally {
+          setLoading(false);
+        }
+      }
+      submit();
+    },
+    [newRepo, repositories]
+  );
 
   return (
     <Container>
@@ -38,8 +49,12 @@ export default function Main() {
           value={newRepo}
           onChange={(e) => setNewRepo(e.target.value)}
         />
-        <SubmitButton>
+        <SubmitButton Loading={loading ? 1 : 0}>
+          {loading ? (
+            <FaSpinner color='#fff' size={14} />
+          ) : (
           <FaPlus color='#fff' size={14} />
+          )}
         </SubmitButton>
       </Form>
     </Container>
